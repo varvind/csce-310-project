9284dd2b69403235b4510357500c7039cf75ebcc
Arvind <varvind@tamu.edu>
2022-04-06 16:49:01 -0500

    Finished postgresql connection and setting up project

--- csce-310-project/app.js
+++ csce-310-project/app.js
@@ -3,11 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'arvind',
+  host: 'localhost',
+  database: 'maroonlink',
+  password: 'arvind00',
+  port: 5432,
+})
+
+
+port = 4000
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,19 +37,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
 
 module.exports = app;
