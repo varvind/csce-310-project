acfe05d7387290705f2e7de3bde06e7c330dec18
Jason Hirsch <49957304+jason-hirsch@users.noreply.github.com>
2022-04-13 17:07:20 -0500

    Admin delete added

--- csce-310-project/routes/admin.js
+++ csce-310-project/routes/admin.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/get:id', function(req, res, next){
-    const userid = req.params.id
-    const adminid = 0
-    res.sendStatus(200).send("{$adminId}")
+router.get('/get/:id', function(req, res, next){
+    const userId = req.params.id
+    const adminId = 0
+    res.status(200).send(`${adminId}`)
 })
 
 router.post('/create', function(req, res, next) {
-    const userId = parseInt(req.body.userid)
+    const { userId } = req.body
     const adminId = 0
-    res.sendStatus(200).send("Admin {$adminId} created for userid {$userId}")
-});
+    res.status(201).json({ userId: userId, adminId: adminId })
+})
+
+router.delete('/delete', function(req, res, next) {
+    const { userId } = req.body
+    const adminId = 0
+    res.status(200).json({ userId: userId, adminId: adminId })
+})
 
 module.exports = router
