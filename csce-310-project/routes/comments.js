d838d3ca8d30da51fcf8ec200690cf6a2f8b0d83
joshuakim135 <joshuakim135@gmail.com>
2022-04-25 16:27:08 -0500

    Changed user_id to pull from url_id instead of session

--- csce-310-project/routes/comments.js
+++ csce-310-project/routes/comments.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.post("/create/comment", (req, res, next) => {
-    if (req.session.userId = null) {
+router.post("/create/comment/:userId", (req, res, next) => {
+    if (req.params.userId = null) {
         res.status(308).send(`Error user not logged in`)
     } else {
         const {event_id, user_comment} = req.body
-        pool.query("Insert INTO event_comments (user_id, event_id, comment) VALUES ($1, $2, $3) RETURNING *", [req.session.userId, event_id, user_comment], () => {
+        pool.query("Insert INTO event_comments (user_id, event_id, comment) VALUES ($1, $2, $3) RETURNING *", [req.params.userId, event_id, user_comment], () => {
             if (error) {
                 throw error
             }
