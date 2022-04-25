5c9815836186452cb2b28441639bdbae8aae78ad
joshuakim135 <joshuakim135@gmail.com>
2022-04-25 17:23:55 -0500

    Began functionality of editing comments

--- csce-310-project/routes/comments.js
+++ csce-310-project/routes/comments.js
@@ -3,9 +3,9 @@ var express = require('express');
 var router = express.Router();
 
 /*
-*   Functionality: Creating a new post and storing it in the database
-*/
-router.post("/create/comment/:userId", (req, res, next) => {
+ *   Functionality: Creating a new post and storing it in the database
+ */
+router.post("/create/:userId", (req, res, next) => {
     // Check if anyone is logged in
     if (req.params.userId = null) {
         res.status(308).send(`Error user not logged in`)
@@ -24,20 +24,31 @@ router.post("/create/comment/:userId", (req, res, next) => {
 })
 
 /*
-*   Functionality: Pull all comments from a user.
-*/
+ *   Functionality: Pull all comments from a user.
+ */
 router.get("/mycomments/:userId", (req, res) => {
     // Check if anyone is logged in
     if (req.params.userId = null) {
         res.status(308).send(`Error user not logged in`)
     } else {
         // Pull comments from database where userid of a row = the userid from the url
-        pool.query(`select comments from Event_Comments where user_id = ${req.params.userId}`)
+        pool.query("select comments from Event_Comments where user_id = " + req.params.userId)
         if (error) {
             throw error
         }
 
-        request = results.rows[0]
-        res.status(201).send(`Successfully pulled all comments from ${request.user_id}`)
+        // request = results.rows[0]
+        res.status(201).send(`Successfully pulled all comments from ${req.params.userId}`)
     }
+})
+
+/*
+ *  Functionality: Allow editing of comments
+ */
+router.post("/update/:userId", (req, res) => {
+    const {old_comment, new_comment} = req.body
+    pool.query("Insert INTO Event_Comments " + new_comment + " where user_id = " + req.params.userId + " and comments = " + old_comment)
+    
+    request = results.row[0]
+    res.status(201).send(`Successfully updated the comment ${old_comment} to ${request.comments}`)
 })
