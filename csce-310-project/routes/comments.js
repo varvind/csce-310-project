5656c752c53da6d647034b4ab2e398d80d3799c0
joshuakim135 <joshuakim135@gmail.com>
2022-04-25 16:35:26 -0500

    comments for next query func

--- csce-310-project/routes/comments.js
+++ csce-310-project/routes/comments.js
@@ -1,10 +1,16 @@
+// Joshua Kim
 var express = require('express');
 var router = express.Router();
 
+/*
+*   Functionality: Creating a new post and storing it in the database
+*/
 router.post("/create/comment/:userId", (req, res, next) => {
+    // Check if anyone is logged in
     if (req.params.userId = null) {
         res.status(308).send(`Error user not logged in`)
     } else {
+        // Insert into database
         const {event_id, user_comment} = req.body
         pool.query("Insert INTO event_comments (user_id, event_id, comment) VALUES ($1, $2, $3) RETURNING *", [req.params.userId, event_id, user_comment], () => {
             if (error) {
@@ -16,3 +22,7 @@ router.post("/create/comment/:userId", (req, res, next) => {
         })
     }
 })
+
+/*
+*   Functionality: Pull all comments from a user.
+*/
