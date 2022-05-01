705c7f6d3253905aa4f25a2281772caeaa246632
joshuakim135 <joshuakim135@gmail.com>
2022-05-01 14:07:52 -0500

    ddd

--- csce-310-project-frontend/src/pages/All_Comments.js
+++ csce-310-project-frontend/src/pages/All_Comments.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const All_Comments = () => {
     return (
-        <></>
+        // empty fragment allows us to return all jsx elements wrapped in one
+        <>
+            
+        </>
     )
 } 
 
